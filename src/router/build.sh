73714ce266d24162f48796b52172e69206fe5b8d
Jeremy Drake <github@jdrake.com>
2014-10-08 18:04:45 -0700

    add error reporting to build.sh.  Add a retry to configure, since it seems to be failing the first time

--- src/router/build.sh
+++ src/router/build.sh
@@ -4,11 +4,11 @@
 cp configs/northstar/.config_northstar .config
 #make -f Makefile.northstar koldconf
 #make -f Makefile.northstar kprep
-make -f Makefile.northstar kernel
-make -f Makefile.northstar install_headers
-make -f Makefile.northstar configure
-make -f Makefile.northstar json-c-clean
-make -f Makefile.northstar json-c-configure
-make -f Makefile.northstar
-make -f Makefile.northstar package
+make -f Makefile.northstar kernel || (echo kernel; false)
+make -f Makefile.northstar install_headers || (echo install_headers; false)
+make -f Makefile.northstar configure || make -f Makefile.northstar configure || (echo configure; false)
+make -f Makefile.northstar json-c-clean || (echo json-c-clean; false)
+make -f Makefile.northstar json-c-configure || (echo json-c-configure; false)
+make -f Makefile.northstar || (echo make; false)
+make -f Makefile.northstar package || (echo package; false)
 
