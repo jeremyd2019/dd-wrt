d0a2b27e19039fd8464d5aeb31a0455faea241ab
Jeremy Drake <github@jdrake.com>
2014-10-08 17:08:22 -0700

    Tweak build.sh with fixes building clean checkout.
    Add 'empty' style dir by adding a .gitignore file, since git doesn't seem to like empty directories.

--- src/router/build.sh
+++ src/router/build.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-make -f Makefile.northstar koldconf
-make -f Makefile.northstar kprep
-make -f Makefile.northstar configure
+./touches.sh
+cp configs/northstar/.config_northstar .config
+#make -f Makefile.northstar koldconf
+#make -f Makefile.northstar kprep
 make -f Makefile.northstar kernel
+make -f Makefile.northstar install_headers
+make -f Makefile.northstar configure
+make -f Makefile.northstar json-c-clean
+make -f Makefile.northstar json-c-configure
 make -f Makefile.northstar
 make -f Makefile.northstar package
 
