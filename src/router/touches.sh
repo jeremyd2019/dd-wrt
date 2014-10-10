f2430ea703476c9698c134fb49625c869c2afa15
Jeremy Drake <github@jdrake.com>
2014-10-10 13:12:29 -0700

    add gd to libs that get their automake/conf/aclocal files touched

--- src/router/touches.sh
+++ src/router/touches.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for dir in glib20/gettext glib20/gettext/gettext-runtime glib20/gettext/gettext-runtime/libasprintf glib20/gettext/gettext-tools; do
+for dir in glib20/gettext glib20/gettext/gettext-runtime glib20/gettext/gettext-runtime/libasprintf glib20/gettext/gettext-tools libgd; do
 	touch $dir/configure $dir/Makefile.in $dir/aclocal.m4
 done
 
